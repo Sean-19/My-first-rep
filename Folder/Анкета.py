6b01b70d6b3e49d9317d78ab2f399698a413b52c
Sean-19 <steil2517@gmail.com>
2020-06-07 18:00:56 +0300

    Хватит на сегодня

---
+++ Folder/Анкета.py
@@ -0,0 +1,7 @@
+print('What is your name?')
+name = input()
+def hi():
+    res = f'Hello {name} how do you do?'
+    print (res + 'print + or -')
+hi ()
+dela = input()
--- Project.py
+++ Project.py
@@ -2,9 +2,9 @@ print ('Hello, what is your name?')
 
 name = input ()
 
-Hi = 'i miss you, {}'
+Hi = 'i miss you, '
 
-result = Hi.format(name)
-#В данном моменте выполняется функция формата
+result = f'{Hi}{name}'
+#В данном моменте выполняется функция формата, Есть вариант формата .format, Есть вариант формата как здесь f'
 print (result)
 
